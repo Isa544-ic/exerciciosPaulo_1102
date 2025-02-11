c1d467b4acef3053c1918fb37fe91447a01f4a55
Isa544-ic <isabellacrivelari20@gmail.com>
2025-02-11 16:53:30 -0300

    Add files via upload

---
+++ exercicio3_Paulo1102/exercicio3_Paulo1102/exercicio3_Paulo1102/Program.cs
@@ -0,0 +1,116 @@
+﻿////ex1
+//static int[] GeraJogo()
+//{
+//    int[] mega = new int[6];
+//    Random r = new Random();
+//    for(int pos=0;pos<6;pos++)
+//    {
+//        int num = r.Next(1,61);
+//        if(mega.Contains(num))
+//        {
+//            pos--;
+//        }
+//        else
+//        {
+//            mega[pos] = num;
+//        }
+//    }
+//    return mega;
+//}
+
+////entradas
+//int qtdjogos;
+//int[,] jogos;
+
+//Console.Write("Informe quantos jogos você deseja: ");
+//qtdjogos = int.Parse(Console.ReadLine());
+
+////processamento e saida
+//Console.WriteLine("O total a ser pago é: R$" + qtdjogos * 5);
+//jogos = new int[qtdjogos, 6];
+//Console.WriteLine("JOGOS GERADOS:");
+//for(int j=0;j<qtdjogos;j++)
+//{
+//    int[] jogo = GeraJogo();
+//    for(int pos=0; pos<6;pos++)
+//    {
+//        Console.Write(jogo[pos] + "\t");
+//        jogos[j,pos] = jogo[pos];
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine("Sorteio real:");
+//int[] megasena = GeraJogo();
+//for (int pos = 0; pos < 6; pos++)
+//{
+//    Console.Write(megasena[pos] + "\t");
+//}
+
+//for(int j=0;j<qtdjogos;j++)
+//{
+//    Console.Write("\nAcertos jogo " + (j + 1) + ": ");
+//    int acertos = 0;
+//    for(int pos=0; pos<6;pos++)
+//    {
+//        if (megasena.Contains(jogos[j,pos]))
+//        {
+//            acertos++;
+//        }
+//    }
+//    Console.Write(acertos);
+//}
+
+
+//ex 2
+//string[] nomes = new string[10];
+//string buscar;
+
+//for (int n = 0; n < 10; n++)
+//{
+//    Console.Write("Informe o nome " + (n+1) + ": ");
+//    nomes[n] = Console.ReadLine();
+//}
+
+//Console.Write("Informe o nome que deseja verificar: ");
+//buscar = Console.ReadLine();
+
+//if(nomes.Contains(buscar))
+//{
+//    Console.WriteLine("Sim, esse foi um nome lido anteriormente!");
+//}
+//else
+//{
+//    Console.WriteLine("Não, não foi um nome lido.");
+//}
+
+//ex3
+double[] precos = new double[30];
+Random r = new Random();
+for (int dia = 0; dia < 30; dia++)
+{
+	precos[dia] = Math.Round(r.NextDouble() * 25 + 25, 2);
+}
+
+//processamento e saida
+double maior = precos[0], menor = precos[0], maiorAlta = 0;
+int diaMaiorAlta = 0;
+for (int dia = 1; dia < 30; dia++)
+{
+	if (precos[dia] > maior)
+		maior = precos[dia];
+	if (precos[dia] < menor)
+		menor = precos[dia];
+	double variacao = precos[dia] - precos[dia - 1];
+	if (variacao > maiorAlta)
+	{
+		maiorAlta = variacao;
+		diaMaiorAlta = dia;
+	}
+}
+
+Console.WriteLine("Maior preço: " + maior +
+	"\nMenor preço: " + menor +
+	"\nMaior variação: " + maiorAlta + " no dia " +
+	(diaMaiorAlta + 1) +
+	"\nRentabilidade: " + (precos[29] - precos[0]));
---
+++ exercicio4_Paulo1102/exercicio4_Paulo1102/exercicio4_Paulo1102/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main()
+	{
+		Random rand = new Random();
+		List<int> lago = new List<int>();
+		List<Player> jogadores = new List<Player>();
+
+		// Definir qntos de peixes no lago
+		Console.Write("Digite o número de peixes no lago (1-50): ");
+		int numPeixes = int.Parse(Console.ReadLine());
+
+		// peixes em posições aleatórias
+		while (lago.Count < numPeixes)
+		{
+			int posicao = rand.Next(1, 51);
+			if (!lago.Contains(posicao))
+				lago.Add(posicao);
+		}
+
+		// Definir qntos de jogadores
+		Console.Write("Digite o número de jogadores: ");
+		int numJogadores = int.Parse(Console.ReadLine());
+
+		for (int i = 0; i < numJogadores; i++)
+		{
+			Console.Write($"Digite o nome do jogador {i + 1}: ");
+			string nome = Console.ReadLine();
+			jogadores.Add(new Player(nome));
+		}
+
+		// Definir número de tentativas por jogador
+		Console.Write("Digite o número de tentativas por jogador: ");
+		int tentativas = int.Parse(Console.ReadLine());
+
+		// Rodadas do jogo
+		for (int t = 0; t < tentativas; t++)
+		{
+			foreach (var jogador in jogadores)
+			{
+				Console.Write($"{jogador.Nome}, escolha uma posição (1-50): ");
+				int tentativa = int.Parse(Console.ReadLine());
+
+				if (lago.Contains(tentativa))
+				{
+					Console.WriteLine("Você pescou um peixe!");
+					jogador.PeixesPescados++;
+					lago.Remove(tentativa);
+				}
+				else
+				{
+					Console.WriteLine("Nenhum peixe nesta posição.");
+				}
+			}
+		}
+
+		// vencedor
+		jogadores.Sort((p1, p2) => p2.PeixesPescados.CompareTo(p1.PeixesPescados));
+		if (jogadores[0].PeixesPescados > jogadores[1].PeixesPescados)
+		{
+			Console.WriteLine($"O vencedor é {jogadores[0].Nome} com {jogadores[0].PeixesPescados} peixes!");
+		}
+		else
+		{
+			Console.WriteLine("Empate ou vitória do pesqueiro!");
+		}
+	}
+}
+
+class Player
+{
+	public string Nome { get; }
+	public int PeixesPescados { get; set; }
+
+	public Player(string nome)
+	{
+		Nome = nome;
+		PeixesPescados = 0;
+	}
+}
